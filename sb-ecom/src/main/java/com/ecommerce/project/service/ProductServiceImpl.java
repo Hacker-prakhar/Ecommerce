d5b45e8e5e19161231743ebcdb9325a788ca6acc
PRAKHAR KUMAR <kumarprakhar73@gmail.com>
2026-08-16 01:38:26 +0530

    Fix typo in setting product image

--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -189,6 +189,8 @@ public class ProductServiceImpl implements ProductService {
         Product product = modelMapper.map(productDTO, Product.class);
 
         productFromDb.setProductName(product.getProductName());
+        
+        productFromDb.setProducImage(product.getImage());
         productFromDb.setDescription(product.getDescription());
         productFromDb.setQuantity(product.getQuantity());
         productFromDb.setDiscount(product.getDiscount());
